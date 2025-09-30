be28016aa8e732b09ff31b28cbe4abae101f326b
lili-QA28 <maly.24.17.02@gmail.com>
2025-09-30 01:02:59 -0400

    Resolviendo observaciones del PR 6

--- tests/boards/test_delete_board.spec.js
+++ tests/boards/test_delete_board.spec.js
@@ -6,16 +6,6 @@ test('Verificar que se elimine el tablero', async ({ trelloPage }) => {
     const boardName = 'Prueba para eliminar un board' ;
 
     await trelloHome.createANewBoard(boardName);
-    
-    await trelloPage.locator('[aria-label="Show menu"]').click();
-    await trelloPage.getByRole('button', { name: /close board|cerrar tablero/i }).click();
-    await trelloPage.getByRole('button', { name: /^close$|^cerrar$/i }).click();
 
-    await expect(trelloPage.getByText(/this board is closed/i)).toBeVisible({ timeout: 10000 });
-
-    await trelloPage.locator('[aria-label="Show menu"]').click();
-    await trelloPage.getByRole('button', { name: /permanently delete board|eliminar permanentemente/i }).click();
-    await trelloPage.getByRole('button', { name: /^delete$|^eliminar$/i }).click();
-
-    //await expect(trelloPage).not.toHaveURL(/\/b\//);
+    await trelloHome.deleteBoard();
 });
