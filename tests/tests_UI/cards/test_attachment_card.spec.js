e803c4cdf7e6d1301c1cd1dbfc32b3bf0b13b857
EJhaelis <erika.soto1610@gmail.com>
2025-10-07 14:54:27 -0400

    Corrigiendo errores 2

--- tests/tests_UI/cards/test_attachment_card.spec.js
+++ tests/tests_UI/cards/test_attachment_card.spec.js
@@ -13,7 +13,7 @@ cardList.describe('Card tests - Attachments', () => {
     cardList('@smoke CD08: Verificar que se puede eliminar un archivo adjunto de una tarjeta', async ({testCard}) => {
         const filePath = path.resolve('./uploads/test.txt');
         const fileName = 'test.txt';
-        await testCard.addAttachment(filePath, 2000);
+        await testCard.addAttachment(filePath, 1500);
         await testCard.removeAttachment(fileName, 1500);
         expect(await testCard.isAttachmentDeleted(fileName)).toBe(true);
         await testCard.closeCard();
